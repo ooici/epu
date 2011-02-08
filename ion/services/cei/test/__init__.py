9eebfefdc1167bfb45dfe4d25b438865679443ff
David LaBissoniere <labisso@uchicago.edu>
2011-02-08 13:31:36 -0600

    DTRS tests and integration

---
+++ ion/services/cei/test/__init__.py
@@ -0,0 +1,15 @@
+
+import os
+
+FIXTURES_ROOT = 'fixtures'
+
+class FileFixtures(object):
+    def __init__(self, subdir=None):
+        test_root = os.path.abspath(os.path.dirname(__file__))
+        self.root = os.path.join(test_root, FIXTURES_ROOT)
+        if subdir:
+            self.root = os.path.join(self.root, subdir)
+        assert os.path.exists(self.root), "No test fixtures?: " + self.root
+
+    def path(self, name):
+        return os.path.join(self.root, name)
