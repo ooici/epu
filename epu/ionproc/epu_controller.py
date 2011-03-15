8c1a70fda808e0bc5bcfab6333aa6115f4b75aa0
Tim Freeman <tim@timfreeman.org>
2011-03-15 10:44:59 -0500

    Pass controller name through

--- epu/ionproc/epu_controller.py
+++ epu/ionproc/epu_controller.py
@@ -4,7 +4,6 @@ import ion.util.ionlog
 log = ion.util.ionlog.getLogger(__name__)
 
 from twisted.internet import defer, reactor
-from twisted.internet.task import LoopingCall
 
 from ion.core.process.service_process import ServiceProcess
 from ion.core.process.process import ProcessFactory
@@ -21,12 +20,15 @@ class EPUControllerService(ServiceProcess):
     """EPU Controller service interface
     """
 
-    declare = ServiceProcess.service_declare(name='epu_controller', version='0.1.0', dependencies=[])
-
     def slc_init(self):
+        self.controller_name = self.spawn_args["controller_name"]
+        EPUControllerService.declare = ServiceProcess.service_declare(name=self.controller_name,
+                                                                      version='0.1.0',
+                                                                      dependencies=[])
+
         self.queue_name_work = self.get_scoped_name("system", self.spawn_args["queue_name_work"])
         extradict = {"queue_name_work":self.queue_name_work}
-        cei_events.event("controller", "init_begin", log, extra=extradict)
+        cei_events.event(self.controller_name, "init_begin", log, extra=extradict)
         self.worker_queue = {self.queue_name_work:{'name_type':'worker'}}
         self.laterinitialized = False
         reactor.callLater(0, self.later_init)
@@ -38,33 +40,20 @@ class EPUControllerService(ServiceProcess):
         else:
             log.info("Using default decision engine: %s" % engineclass)
 
-        self.provisioner_client = ProvisionerClient(self)
-
         if self.spawn_args.has_key("engine_conf"):
             engine_conf = self.spawn_args["engine_conf"]
         else:
             engine_conf = None
-        self.core = ControllerCore(self.provisioner_client, engineclass, conf=engine_conf)
+        self.core = ControllerCore(ProvisionerClient(self), engineclass, conf=engine_conf)
 
         self.core.begin_controlling()
-
-        #TODO right now the controller regularly polls the provisioner for
-        # updates using the query operation. In the future, this call will
-        # come from the provisioner's EPU controller and is designed to
-        # support multiple provisioner instances taking operations off of
-        # a work queue.
-        query_sleep_seconds = 5.0
-        log.debug('Starting provisioner query loop - %s second interval',
-                query_sleep_seconds)
-        self.query_loop = LoopingCall(self.provisioner_client.query)
-        self.query_loop.start(query_sleep_seconds, now=False)
 
     @defer.inlineCallbacks
     def later_init(self):
         yield bootstrap.declare_messaging(self.worker_queue)
         self.laterinitialized = True
         extradict = {"queue_name_work":self.queue_name_work}
-        cei_events.event("controller", "init_end", log, extra=extradict)
+        cei_events.event(self.controller_name, "init_end", log, extra=extradict)
 
     def op_heartbeat(self, content, headers, msg):
         self.core.new_heartbeat(content)
@@ -90,6 +79,7 @@ def start(container, starttype, *args, **kwargs):
 
     conf = ioninit.config(__name__)
     spawnargs = {'queue_name_work' : conf['queue_name_work'],
+                 'controller_name': conf['controller_name'],
                  'engine_class' : conf.getValue('engine_class'),
                  'engine_conf' : conf.getValue('engine_conf')}
 
