1584f1f0f289543866cd8fd404a36dd3947b5c8e
David LaBissoniere <labisso@uchicago.edu>
2011-04-15 17:51:24 -0500

    Fixed up deferred handling in test_torque
    Messages are also apparently not always arriving in order..

--- epu/test/test_torque.py
+++ epu/test/test_torque.py
@@ -6,6 +6,7 @@
 @brief Test TorqueManagerService behavior
 """
 
+from collections import deque
 from twisted.internet import defer
 
 from ion.core.process.process import Process
@@ -51,32 +52,37 @@ class TestTorqueManagerService(IonTestCase):
         yield client.watch_queue(sub1Id, "stat", "q1")
         self.assertTrue(self.service.loop.running)
         yield self.service._do_poll()
-        stat = yield sub1.deferred
-        self.assertQueueStat(stat, "q1", 0)
-        status = yield sub1.deferred
-        self.assertQueueStat(status, "q1", '')
+        messages = yield sub1.get_messages(2)
+        self.assertBoth(messages, "q1", 0, '')
 
         self.pbs.set_queue_length("q1", 3)
         self.pbs.set_worker_status("localhost", "down")
         yield self.service._do_poll()
-        stat = yield sub1.deferred
-        self.assertQueueStat(stat, "q1", 3)
-        status = yield sub1.deferred
-        self.assertWorkerStatus(status, "q1", "localhost:down")
+        messages = yield sub1.get_messages(2)
+        self.assertBoth(messages, "q1", 3, "localhost:down")
 
         self.pbs.clear()
         self.pbs.set_queue_length("q1", 5)
         self.pbs.set_worker_status("localhost", "job-exclusive")
         yield self.service._do_poll()
-        stat = yield sub1.deferred
-        self.assertQueueStat(stat, "q1", 5)
-        status = yield sub1.deferred
-        self.assertWorkerStatus(status, "q1", "localhost:job-exclusive")
+        messages = yield sub1.get_messages(2)
+        self.assertBoth(messages, "q1", 5, "localhost:job-exclusive")
 
         # unsubscribe and loop should stop
         yield client.unwatch_queue(sub1Id, "stat", "q1")
         self.assertFalse(self.service.loop.running)
         self.assertFalse(self.service.watched_queues)
+
+    def assertBoth(self, messages, name, length, status):
+        self.assertEqual(2, len(messages))
+        m1,m2 = messages
+        if "queue_length" in m1:
+            self.assertQueueStat(m1, name, length)
+            self.assertWorkerStatus(m2, name, status)
+        else:
+            self.assertQueueStat(m2, name, length)
+            self.assertWorkerStatus(m1, name, status)
+
 
     def assertQueueStat(self, stat, name, length):
         self.assertEqual(stat['queue_name'], name)
@@ -136,6 +142,18 @@ class TestSubscriber(Process):
         self.queue_length = {}
         self.recv_count = {}
         self.deferred = defer.Deferred()
+        self.queue = deque([self.deferred])
+
+    def get_message(self):
+        return self.queue.popleft()
+
+    @defer.inlineCallbacks
+    def get_messages(self, count):
+        l = []
+        for i in range(count):
+            m = yield self.get_message()
+            l.append(m)
+        defer.returnValue(l)
 
     def op_stat(self, content, headers, msg):
         q = content['queue_name']
@@ -146,5 +164,7 @@ class TestSubscriber(Process):
 
         count = self.recv_count.get(q, None)
         self.recv_count[q] = count + 1 if count else 1
-        self.deferred.callback(content)
+        d = self.deferred
         self.deferred = defer.Deferred()
+        self.queue.append(self.deferred)
+        d.callback(content)
