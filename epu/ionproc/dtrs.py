d6711322249cbea330f2cd92ceb5843710fcd08d
Brian Fox <bfox@ucsd.edu>
2011-06-30 08:51:20 -0700

    Hid passwords from logging.

--- epu/ionproc/dtrs.py
+++ epu/ionproc/dtrs.py
@@ -48,8 +48,13 @@ class DeployableTypeRegistryService(ServiceProcess):
     def op_lookup(self, content, headers, msg):
         """Resolve a deployable type
         """
+        
+        # hide the password so it doesn't get logged
+        hide_password = deepcopy(content)
+	if 'cassandra_password' in hide_password:
+            hide_password['cassandra_password'] = '******' 
 
-        log.debug('Received DTRS lookup. content: ' + str(content))
+        log.debug('Received DTRS lookup. content: ' + str(hide_password))
         # just using a file for this right now, to keep it simple
         dt_id = content['deployable_type']
         nodes = content.get('nodes')
@@ -106,7 +111,12 @@ class DeployableTypeRegistryService(ServiceProcess):
                     'iaas_sshkeyname' : site_node.get('sshkeyname'),
                     }
 
-        log.debug('Sending DTRS response: ' + str(result))
+        # hide the password so it doesn't get logged
+        hide_password = deepcopy(result)
+        if 'cassandra_password' in hide_password:
+            hide_password['cassandra_password'] = '******'
+
+        log.debug('Sending DTRS response: ' + str(hide_password))
 
         return self.reply_ok(msg, result)
 
