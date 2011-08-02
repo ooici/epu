a503f62ffd58e65a88a02d57b6b8373afb019c36
David LaBissoniere <labisso@uchicago.edu>
2011-08-02 15:01:48 -0500

    Added another test for reconfigure persistence
    Per #OOIION-45

--- epu/test/test_epu_controller.py
+++ epu/test/test_epu_controller.py
@@ -2,7 +2,9 @@ from ion.util import procutils
 from twisted.internet import defer
 
 from ion.test.iontest import IonTestCase
+from epu.epucontroller.controller_store import ControllerStore
 from epu.ionproc.epu_controller import EPUControllerService
+from epu.ionproc.epu_controller_client import EPUControllerClient
 
 class EPUControllerServiceTest(IonTestCase):
     @defer.inlineCallbacks
@@ -33,7 +35,6 @@ class EPUControllerServiceTest(IonTestCase):
         yield procutils.asleep(1.1)
         self.assertTrue(controller.core.control_loop is not None)
 
-
     @defer.inlineCallbacks
     def test_no_workqueue(self):
         spawnargs = {}
@@ -42,3 +43,38 @@ class EPUControllerServiceTest(IonTestCase):
         controller_id = yield self._spawn_process(controller)
         self.assertEqual(controller.queue_name_work, None)
 
+    @defer.inlineCallbacks
+    def test_reconfigure_recovery(self):
+
+        # test that engine config is correctly using reconfigured engine values
+        # in subsequent starts of the controller. Strategy is:
+        #   1. Start up a controller with some config
+        #   2. Call reconfigure with some more config
+        #   3. Kill controller and restart it, verify engine got the merged
+        #      config.
+
+        store = ControllerStore()
+        spawnargs = {'engine_class' : 'epu.epucontroller.test.test_controller_core.FakeEngine',
+                     'engine_conf' : {'a' : 'a1', 'b' : 'b1'},
+                     'servicename' : 'TestEPUController',
+                     'store' : store}
+        controller = EPUControllerService(spawnargs=spawnargs)
+        self.controller = controller
+        yield self._spawn_process(controller)
+
+        client = EPUControllerClient(targetname='TestEPUController')
+        yield client.attach()
+
+        yield procutils.asleep(1.01)
+
+        yield client.reconfigure_rpc({'a' : 'a2', 'c' : 'c1'})
+        yield procutils.asleep(1.01)
+        controller.terminate()
+
+        controller = EPUControllerService(spawnargs=spawnargs)
+        self.controller = controller
+        yield self._spawn_process(controller)
+        yield procutils.asleep(1.01)
+
+        self.assertEqual(controller.core.engine.initialize_conf,
+                {'a' : 'a2', 'b': 'b1', 'c' : 'c1'})
