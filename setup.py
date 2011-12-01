4d9d7add602829045dc4eadfb4d14933c5dedcc7
David LaBissoniere <labisso@gmail.com>
2011-12-01 17:36:37 -0600

    Removed old queuestat service and tests

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ if sys.version_info < (2, 6, 0):
 setupdict['install_requires'] += ['simplejson==2.1.2',
                                   'httplib2==0.7.1',
                                   'nimboss==0.4.6',
-                                  'txrabbitmq==0.5',
                                   'apache-libcloud==0.5.2',
                                   'ioncore<1.2']
 
