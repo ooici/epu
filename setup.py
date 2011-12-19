44a022b0691294cf563d567a0f48bd274633fa4c
Jamie Chen <jachen@ucsd.edu>
2011-12-19 13:34:13 -0800

    Add osx include path hack

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@
 """
 
 import sys
+import os
+
+# Add /usr/local/include to the path for macs, fixes easy_install for several packages (like gevent and pyyaml)
+if sys.platform == 'darwin':
+    os.environ['C_INCLUDE_PATH'] = '/usr/local/include'
 
 setupdict = {
     'name' : 'epu',
