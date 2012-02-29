a20462354c4c958d689c88cbf24e34606a47a860
BuzzTroll <buzztroll@gmail.com>
2012-02-28 15:13:06 -1000

    work around for kombu bug

--- epu/dashiproc/epumanagement.py
+++ epu/dashiproc/epumanagement.py
@@ -1,3 +1,9 @@
+# Work around for a kombu bug
+from amqplib.client_0_8 import transport
+XXX = transport.SSLTransport
+import kombu.transport.amqplib
+setattr(transport, 'SSLTransport', XXX)
+
 import logging
 
 from dashi import bootstrap
