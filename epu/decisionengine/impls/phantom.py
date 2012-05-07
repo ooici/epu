90cec71af222671515529c58912d10e912aac359
BuzzTroll <buzztroll@gmail.com>
2012-05-07 12:14:07 -1000

    updating phantom plan to refelect current decision engine

--- epu/decisionengine/impls/phantom.py
+++ epu/decisionengine/impls/phantom.py
@@ -1,86 +1,100 @@
 import logging
 import random
 
-from epu.epumanagement.conf import CONF_IAAS_SITE, CONF_IAAS_ALLOCATION
-
 from epu.decisionengine import Engine
 from epu.states import InstanceState
 
 log = logging.getLogger(__name__)
 
-CONF_PRESERVE_N = "preserve_n"
-CONF_DEPLOYABLE_TYPE = "deployable_type"
-CONF_IAAS_IMAGE = "epuworker_image_id"
-
-# Engine-conf key for a list of node IDs that the client would prefer be killed first
-CONF_RETIRABLE_NODES = "retirable_nodes"
-
 BAD_STATES = [InstanceState.TERMINATING, InstanceState.TERMINATED, InstanceState.FAILED]
 
-class PhantomEngine(Engine):
-    """
-    A decision engine based on the needy engine for phantom.
-    """
-    def __init__(self):
-        super(PhantomEngine, self).__init__()
-        self.preserve_n = 0
-        self.iaas_site = None
-        self.iaas_allocation = None
-        self.iaas_image = None
-        self.deployable_type = None
-        self.retirable_nodes = []
+CONF_PRESERVE_N = "preserve_n"
 
-        # For tests.  This information could be logged, as well.
-        self.initialize_count = 0
-        self.initialize_conf = None
-        self.decide_count = 0
-        self.reconfigure_count = 0
+class PhantomEngine(Engine):
+    """A decision engine that maintains N instances of the compensating units.
+    It's Npreserving policy (only) can be reconfigured.
+    It has zero other features.  It's good for tests.
+    """
+
+    def __init__(self):
+        super(SimplestEngine, self).__init__()
+        self.preserve_n = 0
+        self.available_allocations = ["small"]
+        self.available_sites = ["ec2-east"]
+        self.available_types = ["epu_work_consumer"]
 
         self._next_launch_attempt = 0
         self._launch_delay = 1
         self._failed_count = 0
         self._max_launch_delay = 60
         self._launch_count = 0
-        
-
-    def _set_conf(self, newconf):
-        if not newconf:
-            raise ValueError("requires engine conf")
-        if newconf.has_key(CONF_PRESERVE_N):
-            new_n = int(newconf[CONF_PRESERVE_N])
-            if new_n < 0:
-                raise ValueError("cannot have negative %s conf: %d" % (CONF_PRESERVE_N, new_n))
-            self.preserve_n = new_n
-        if newconf.has_key(CONF_IAAS_SITE):
-            self.iaas_site = newconf[CONF_IAAS_SITE]
-        if newconf.has_key(CONF_IAAS_ALLOCATION):
-            self.iaas_allocation = newconf[CONF_IAAS_ALLOCATION]
-        if newconf.has_key(CONF_DEPLOYABLE_TYPE):
-            self.deployable_type = newconf[CONF_DEPLOYABLE_TYPE]
-        if newconf.has_key(CONF_RETIRABLE_NODES):
-            self.retirable_nodes = newconf[CONF_RETIRABLE_NODES]
-        if newconf.has_key(CONF_IAAS_IMAGE):
-            self.iaas_image = newconf[CONF_IAAS_IMAGE]
+        self.decide_count = 0
 
     def initialize(self, control, state, conf=None):
-        self._set_conf(conf)
-        log.info("%s initialized" % __name__)
-        self.initialize_count += 1
-        self.initialize_conf = conf
+        """
+        Give the engine a chance to initialize.  The current state of the
+        system is given as well as a mechanism for the engine to offer the
+        controller input about how often it should be called.
+
+        @note Must be invoked and return before the 'decide' method can
+        legally be invoked.
+
+        @param control instance of Control, used to request changes to system
+        @param state instance of State, used to obtain any known information
+        @param conf None or dict of key/value pairs
+        @exception Exception if engine cannot reach a sane state
+
+        """
+        if not conf:
+            raise ValueError("requires engine conf")
+        
+        if conf.has_key("force_site"):
+            self.available_sites = [conf["force_site"]]
+
+        if conf.has_key("epuworker_type"):
+            self.available_types = [conf["epuworker_type"]]
+
+        if conf.has_key("epuworker_allocation"):
+            self.available_allocations = [conf["epuworker_allocation"]]
+
+        if conf.has_key(CONF_PRESERVE_N):
+            self.preserve_n = int(conf[CONF_PRESERVE_N])
+            if self.preserve_n < 0:
+                raise ValueError("cannot have negative %s conf: %d" % (CONF_PRESERVE_N, self.preserve_n))
+        else:
+            raise ValueError("requires %s conf: %d" % (CONF_PRESERVE_N, self.preserve_n))
+
+        log.info("Simplest-engine initialized, preserve_n: %d" % self.preserve_n)
 
     def dying(self):
-        pass
+        raise Exception("Dying not implemented on the simplest decision engine")
 
     def decide(self, control, state):
+        """
+        Give the engine a chance to act on the current state of the system.
+
+        @note May only be invoked once at a time.
+        @note When it is invoked is up to EPU Controller policy and engine
+        preferences, see the decision engine implementer's guide.
+
+        @param control instance of Control, used to request changes to system
+        @param state instance of State, used to obtain any known information
+        @retval None
+        @exception Exception if the engine has been irrevocably corrupted
+
+        """
         all_instances = state.instances.values()
         valid_set = set(i.instance_id for i in all_instances if not i.state in BAD_STATES)
-
+        
         #check all nodes to see if some are unhealthy, and terminate them
         for instance in state.get_unhealthy_instances():
-            log.warn("Phantom Terminating unhealthy node: %s", instance.instance_id)
+            log.warn("Terminating unhealthy node: %s", instance.instance_id)
             self._destroy_one(control, instance.instance_id)
             # some of our "valid" instances above may be unhealthy
             valid_set.discard(instance.instance_id)
+
+        # How many instances are not terminated/ing or corrupted?
+        valid_count = len(valid_set)
 
         failed_array = set(i.instance_id for i in all_instances if i.state == InstanceState.FAILED)
         failed_count = len(failed_array)
@@ -95,31 +109,21 @@ class PhantomEngine(Engine):
                 self._launch_delay = self._max_launch_delay
             self._next_launch_attempt = self.decide_count + self._launch_delay
 
-        # How many instances are not terminated/ing or corrupted?
         valid_count = len(valid_set)
 
         force_pending = True
         if valid_count == self.preserve_n:
-            log.debug("Phantom valid count (%d) = target (%d)" % (valid_count, self.preserve_n))
+            log.debug("valid count (%d) = target (%d)" % (valid_count, self.preserve_n))
             force_pending = False
-            # reset delay
-            self._launch_delay = 1
         elif valid_count < self.preserve_n:
-            log.debug("Phantom valid count (%d) < target (%d)" % (valid_count, self.preserve_n))
+            log.debug("valid count (%d) < target (%d)" % (valid_count, self.preserve_n))
             while valid_count < self.preserve_n:
-                self._launch_one(control, uniquekv={CONF_IAAS_IMAGE: self.iaas_image})
+                self._launch_one(control)
                 valid_count += 1
         elif valid_count > self.preserve_n:
-            log.debug("Phantom valid count (%d) > target (%d)" % (valid_count, self.preserve_n))
+            log.debug("valid count (%d) > target (%d)" % (valid_count, self.preserve_n))
             while valid_count > self.preserve_n:
-                die_id = None
-                for instance_id in valid_set:
-                    # Client would prefer that one of these is terminated
-                    if instance_id in self.retirable_nodes:
-                        die_id = instance_id
-                        break
-                if not die_id:
-                    die_id = random.sample(valid_set, 1)[0] # len(valid_set) is always > 0 here
+                die_id = random.sample(valid_set, 1)[0] # len(valid_set) is always > 0 here
                 self._destroy_one(control, die_id)
                 valid_set.discard(die_id)
                 valid_count -= 1
@@ -128,35 +132,28 @@ class PhantomEngine(Engine):
             self._set_state_pending()
         else:
             self._set_state(all_instances, -1, health_not_checked=control.health_not_checked)
-
         self.decide_count += 1
-
+            
     def _launch_one(self, control, uniquekv=None):
-        if not self.iaas_site:
-            raise Exception("No IaaS site configuration")
-        if not self.iaas_allocation:
-            raise Exception("No IaaS allocation configuration")
-        if not self.iaas_image:
-            raise Exception("No IaaS image configuration")
-        if not self.deployable_type:
-            raise Exception("No deployable type configuration")
-
         log.debug("Phantom Next launch will be at %d, currently at %d" % (self._next_launch_attempt, self.decide_count))
         if self._next_launch_attempt > self.decide_count:
             log.info("Phantom Skipping this launch due to back off.  Next launch will be at %d, currently at %d" % (self._next_launch_attempt, self.decide_count))
         else:
-            launch_id, instance_ids = control.launch(self.deployable_type,
-                self.iaas_site, self.iaas_allocation, extravars=uniquekv)
+
+            owner = control.domain.owner
+            launch_id, instance_ids = control.launch(self.available_types[0],
+                self.available_sites[0], self.available_allocations[0],
+                extravars=uniquekv, caller=owner)
             if len(instance_ids) != 1:
                 raise Exception("Could not retrieve instance ID after launch")
-            self._launch_count = self._launch_count + 1
             log.info("Phantom Launched an instance ('%s').  decide count %d, next_attempt %d", instance_ids[0], self.decide_count, self._next_launch_attempt)
             self._next_launch_attempt = 0
 
     def _destroy_one(self, control, instanceid):
-        control.destroy_instances([instanceid])
+        owner = control.domain.owner
+        control.destroy_instances([instanceid], caller=owner)
         log.info("Destroyed an instance ('%s')" % instanceid)
-
+        
     def reconfigure(self, control, newconf):
         """
         Give the engine a new configuration.
@@ -171,7 +168,10 @@ class PhantomEngine(Engine):
 
         """
         if not newconf:
-            raise ValueError("reconfigure expects new engine conf")
-        self._set_conf(newconf)
-        log.info("%s reconfigured" % __name__)
-        self.reconfigure_count += 1
+            raise ValueError("expected new engine conf")
+        log.debug("engine reconfigure, newconf: %s" % newconf)
+        if newconf.has_key(CONF_PRESERVE_N):
+            new_n = int(newconf[CONF_PRESERVE_N])
+            if new_n < 0:
+                raise ValueError("cannot have negative %s conf: %d" % (CONF_PRESERVE_N, new_n))
+            self.preserve_n = new_n
