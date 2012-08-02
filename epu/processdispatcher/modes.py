9f7c3ed1f713dda07e90303121929e68d051a17c
Patrick Armstrong <patricka@uvic.ca>
2012-08-02 09:59:06 -0700

    Remove unused methods from PD mode classes

--- epu/processdispatcher/modes.py
+++ epu/processdispatcher/modes.py
@@ -5,21 +5,9 @@ class QueueingMode(object):
     START_ONLY = "START_ONLY"
     RESTART_ONLY = "RESTART_ONLY"
 
-    _str_map = {1: 'NEVER', 2: 'ALWAYS', 3: 'START_ONLY', 4: 'RESTART_ONLY'}
-
-    @classmethod
-    def from_pyon_enum(value):
-        return QueueingMode._str_map[value]
-
 
 class RestartMode(object):
 
     NEVER = "NEVER"
     ALWAYS = "ALWAYS"
     ABNORMAL = "ABNORMAL"
-    _str_map = {1: 'NEVER', 2: 'ALWAYS', 3: 'ABNORMAL'}
-
-    @classmethod
-    def from_pyon_enum(value):
-        return RestartMode._str_map[value]
-
