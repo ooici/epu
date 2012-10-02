dd1b7e5f10f3d53b510a9c917860a7d9c17799d9
Patrick Armstrong <patricka@uvic.ca>
2012-10-02 12:54:18 -0700

    Add basic CloudWatch Adapter

---
+++ epu/sensors/test/test_cloudwatch.py
@@ -0,0 +1,41 @@
+
+import os
+
+from nose.plugins.skip import SkipTest
+from datetime import datetime, timedelta
+
+from epu.sensors import Statistics
+from epu.sensors.cloudwatch import CloudWatch
+
+class TestRealCloudWatch(object):
+
+    def setup(self):
+
+        access_key = os.environ.get("EC2_ACCESS_KEY")
+        secret_key = os.environ.get("EC2_SECRET_KEY")
+        self.instance = os.environ.get("CLOUDWATCH_TEST_INSTANCE")
+        self.mock_traffic_sentinel = False
+
+        if not (access_key and secret_key and self.instance):
+            raise SkipTest("CloudWatch access key, secret key or instance aren't in env")
+
+        self.cloudwatch = CloudWatch(access_key, secret_key)
+
+    def teardown(self):
+        pass
+
+    def test_get_metric_statistics(self):
+
+        period = 60
+        end_time = datetime.utcnow()
+        start_time = end_time - timedelta(minutes=2)
+        metric_name = "CPUUtilization"
+        statistics = Statistics.AVERAGE
+        dimensions = {'InstanceId': [self.instance]}
+
+        result = self.cloudwatch.get_metric_statistics(period, start_time,
+                end_time, metric_name, statistics, dimensions)
+
+        assert len(result) > 0
+        assert result.get(self.instance)
+        assert result[self.instance].get(Statistics.AVERAGE)
