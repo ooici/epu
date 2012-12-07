e98349df9697e257030f73291a6660e34104ab89
Pierre Riteau <priteau@ci.uchicago.edu>
2012-12-07 13:11:50 -0600

    Remove success messages, they are moving to ceiclient

--- epu/dashiproc/dtrs.py
+++ epu/dashiproc/dtrs.py
@@ -81,8 +81,7 @@ class DTRS(object):
     # Deployable Types
 
     def add_dt(self, caller, dt_name, dt_definition):
-        self.core.store.add_dt(caller, dt_name, dt_definition)
-        return "Added DT %s for user %s" % (dt_name, caller)
+        return self.core.store.add_dt(caller, dt_name, dt_definition)
 
     def describe_dt(self, caller, dt_name):
         return self.core.describe_dt(caller, dt_name)
@@ -91,18 +90,15 @@ class DTRS(object):
         return self.core.store.list_dts(caller)
 
     def remove_dt(self, caller, dt_name):
-        self.core.store.remove_dt(caller, dt_name)
-        return "Removed DT %s for user %s" % (dt_name, caller)
+        return self.core.store.remove_dt(caller, dt_name)
 
     def update_dt(self, caller, dt_name, dt_definition):
-        self.core.store.update_dt(caller, dt_name, dt_definition)
-        return "Updated DT %s for user %s" % (dt_name, caller)
+        return self.core.store.update_dt(caller, dt_name, dt_definition)
 
     # Sites
 
     def add_site(self, site_name, site_definition):
-        self.core.store.add_site(site_name, site_definition)
-        return "Added site %s" % site_name
+        return self.core.store.add_site(site_name, site_definition)
 
     def describe_site(self, site_name):
         return self.core.describe_site(site_name)
@@ -111,18 +107,15 @@ class DTRS(object):
         return self.core.store.list_sites()
 
     def remove_site(self, site_name):
-        self.core.store.remove_site(site_name)
-        return "Removed site %s" % site_name
+        return self.core.store.remove_site(site_name)
 
     def update_site(self, site_name, site_definition):
-        self.core.store.update_site(site_name, site_definition)
-        return "Updated site %s" % site_name
+        return self.core.store.update_site(site_name, site_definition)
 
     # Credentials
 
     def add_credentials(self, caller, site_name, site_credentials):
-        self.core.add_credentials(caller, site_name, site_credentials)
-        return "Added credentials of site %s for user %s" % (site_name, caller)
+        return self.core.add_credentials(caller, site_name, site_credentials)
 
     def describe_credentials(self, caller, site_name):
         return self.core.describe_credentials(caller, site_name)
@@ -131,12 +124,11 @@ class DTRS(object):
         return self.core.store.list_credentials(caller)
 
     def remove_credentials(self, caller, site_name):
-        self.core.store.remove_credentials(caller, site_name)
-        return "Removed credentials of site %s for user %s" % (site_name, caller)
+        return self.core.store.remove_credentials(caller, site_name)
 
     def update_credentials(self, caller, site_name, site_credentials):
-        self.core.store.update_credentials(caller, site_name, site_credentials)
-        return "Updated credentials of site %s for user %s" % (site_name, caller)
+        return self.core.store.update_credentials(caller, site_name,
+                                                  site_credentials)
 
     # Old DTRS methods - keeping the API unmodified for now
 
