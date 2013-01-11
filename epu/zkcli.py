d4b5db5be783e34f79d745a804cfc7a8eac3df1b
David LaBissoniere <labisso@gmail.com>
2013-01-11 17:43:45 -0600

    Add epu-zktool script for ZK management in boot

---
+++ epu/zkcli.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+import yaml
+import os
+
+from kazoo.client import KazooClient
+from kazoo.exceptions import NodeExistsException
+
+from epu import zkutil
+from epu.processdispatcher.store import ProcessDispatcherZooKeeperStore
+
+
+class CliError(Exception):
+    pass
+
+
+def get_kazoo(zk_config):
+    hosts = zk_config.get('hosts')
+    if not hosts:
+        raise CliError("ZK config missing hosts")
+    username = zk_config.get('username')
+    password = zk_config.get('password')
+    if (username or password) and not (username and password):
+        raise CliError("Both username and password must be specified, or neither")
+    kwargs = zkutil.get_kazoo_kwargs(username, password)
+    return KazooClient(hosts, **kwargs)
+
+
+def get_path(zk_config, args):
+    path = args.path or zk_config.get('path')
+    if not path:
+        raise CliError("path must be specified on command line or in config")
+    if not path.startswith("/"):
+        raise CliError("ZK path does not begin with '/'?")
+    return path
+
+
+def cmd_setup(zk_config, args):
+    path = get_path(zk_config, args)
+    kazoo = get_kazoo(zk_config)
+
+    kazoo.start()
+    kazoo.ensure_path(path)
+
+    # create this special path in the store to tell the Process Dispatcher
+    # to start up in SYSTEM BOOT mode. This will prevent it from automatically
+    # rescheduling any processes until the boot has ended.
+
+    # WARNING: pd path is hardcoded here!
+    system_boot_path = "%s/pd/%s" % (path, ProcessDispatcherZooKeeperStore.SYSTEM_BOOT_PATH)
+    system_boot_path = os.path.normpath(system_boot_path)
+
+    try:
+        kazoo.create(system_boot_path, "", makepath=True)
+    except NodeExistsException:
+        pass
+    kazoo.stop()
+
+
+def cmd_destroy(zk_config, args):
+    path = get_path(zk_config, args)
+    kazoo = get_kazoo(zk_config)
+
+    kazoo.start()
+    kazoo.delete(path, recursive=True)
+    kazoo.stop()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='EPU ZooKeeper management utility')
+    parser.add_argument('--config', '-c', metavar="config.yml",
+        type=argparse.FileType('r'), required=True)
+
+    subparsers = parser.add_subparsers()
+    setup_parser = subparsers.add_parser("setup",
+        description=cmd_setup.__doc__, help="prepare path for system launch")
+    setup_parser.add_argument("path", help="ZooKeeper base path", nargs='?')
+    setup_parser.set_defaults(func=cmd_setup)
+
+    destroy_parser = subparsers.add_parser("destroy",
+        description=cmd_destroy.__doc__, help="delete path")
+    destroy_parser.add_argument("path", help="ZooKeeper base path", nargs='?')
+    destroy_parser.set_defaults(func=cmd_destroy)
+
+    args = parser.parse_args()
+    config = yaml.load(args.config)
+
+    if not zkutil.is_zookeeper_enabled(config):
+        print >>sys.stderr, "ZooKeeper is not enabled in config. Doing nothing."
+        return
+
+    zk_config = zkutil.get_zookeeper_config(config)
+    try:
+        args.func(zk_config, args)
+    except CliError, e:
+        parser.error(str(e))
+
+
+if __name__ == '__main__':
+    main()
