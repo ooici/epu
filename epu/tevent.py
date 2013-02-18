e4e8c90791c19bdff2c8f36cc94150a79b2e5613
Patrick Armstrong <patricka@uvic.ca>
2013-02-18 12:42:38 -0800

    Add fail_fast to provisioner threads.
    If a provisioner thread dies, it will take down the whole provisioner
    process, which should help with debugging and reliability when deployed
    with supervisord

--- epu/tevent.py
+++ epu/tevent.py
@@ -1,6 +1,9 @@
+import os
+import sys
+import socket
 import weakref
+import traceback
 import threading
-import signal
 from multiprocessing.pool import ThreadPool
 """
 Helper functions for working with stdlib threading library
@@ -8,13 +11,38 @@ Helper functions for working with stdlib threading library
 Inspired by the gevent api
 """
 
-def spawn(func, *args, **kwargs):
+def spawn(func, fail_fast=False, exit=None, *args, **kwargs):
+    """spawn - spawn and start a thread
+
+    @param func - function to run in the thread
+    @param fail_fast - if set to True, if this thread raises an
+        unhandled exception, it will terminate the parent process
+    @param exit - alternate exit function to call to bring down
+        the process
+    """
     if hasattr(func, 'im_class'):
         name = "%s.%s" % (func.im_class.__name__, func.__name__)
     else:
         name = func.__name__
 
-    _thread = threading.Thread(target=func, name=name, args=tuple(args), kwargs=kwargs)
+    if fail_fast == True:
+        def critical_wrapper():
+            try:
+                func()
+            except (Exception, socket.timeout) as e:
+                msg = "%s Thread has died, but it is critical. Exiting." % name
+                print >> sys.stderr, msg
+                if exit is None:
+                    traceback.print_exc()
+                    os._exit(os.EX_SOFTWARE)
+                else:
+                    exit()
+
+        _func = critical_wrapper
+    else:
+        _func = func
+
+    _thread = threading.Thread(target=_func, name=name, args=tuple(args), kwargs=kwargs)
     _thread.daemon = True
     _thread.start()
     return _thread
