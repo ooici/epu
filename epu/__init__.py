347b7c462544f86f03a42fa923ef6abf3e6eabb6
David LaBissoniere <david@labisso.com>
2013-04-29 15:28:42 -0500

    Remove old workaround for gevent < 0.13.7

--- epu/__init__.py
+++ epu/__init__.py
@@ -1,19 +1,9 @@
 from os import environ
 import logging
 
-
-def patch_extras():
-    # workaround for http://code.google.com/p/gevent/issues/detail?id=112
-    # gevent isn't patching threading._sleep which causes problems
-    # for Condition objects
-    from gevent import sleep
-    import threading
-    threading._sleep = sleep
-
 if environ.get('EPU_USE_GEVENT'):
     from gevent import monkey
     monkey.patch_all()
-    patch_extras()
 
 if not environ.get('AMQP_DEBUG_LOGGING'):
     amqp_logger = logging.getLogger("amqp")
