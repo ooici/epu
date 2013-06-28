ooici/epu

commit 8a876418706397f09024bdc17023caee72ee2a10

Automatically retry PD->EEAgent launch requests
OOIION-969

epu/processdispatcher/util.py
@@ -20,3 +20,30 @@ def get_process_state_message(process):
 
     return "Process %s%s -> %s round=%s%s" % (process.upid, name,
         process.state, process.round, location)
+
+
+def get_set_difference(set1, set2):
+    """Returns a tuple of lists (added, removed)
+    """
+    added = list(set1.difference(set2))
+    removed = list(set2.difference(set1))
+    return added, removed
+
+
+def get_set_difference_debug_message(set1, set2):
+    """Utility function for building log messages about set content changes
+    """
+    try:
+        difference1, difference2 = get_set_difference(set1, set2)
+    except Exception, e:
+        return "can't calculate set difference. are these really sets?: %s" % str(e)
+
+    if difference1 and difference2:
+        return "removed=%s added=%s" % (difference1, difference2)
+    elif difference1:
+        return "removed=%s" % (difference1,)
+    elif difference2:
+        return "added=%s" % (difference2,)
+    else:
+        return "sets are equal"
+

epu/config/processdispatcher.yml
@@ -2,3 +2,4 @@ processdispatcher:
   engines: {}
   restart_throttling_config:
     minimum_time_between_starts: 2
+  dispatch_retry_seconds: 30

epu/dashiproc/processdispatcher.py
@@ -72,11 +72,12 @@ class ProcessDispatcherService(object):
 
         launch_type = self.CFG.processdispatcher.get('launch_type', 'supd')
         restart_throttling_config = self.CFG.processdispatcher.get('restart_throttling_config', {})
+        dispatch_retry_seconds = self.CFG.processdispatcher.get('dispatch_retry_seconds')
 
         self.matchmaker = PDMatchmaker(self.core, self.store, self.eeagent_client,
             self.registry, self.epum_client, self.notifier, self.topic,
             domain_definition_id, base_domain_config, launch_type,
-            restart_throttling_config)
+            restart_throttling_config, dispatch_retry_seconds)
 
         self.doctor = PDDoctor(self.core, self.store, config=self.CFG)
         self.ready_event = threading.Event()

epu/processdispatcher/core.py
@@ -7,7 +7,8 @@ from epu.util import is_valid_identifier, parse_datetime, ceiling_datetime
 from epu.processdispatcher.store import ProcessRecord, NodeRecord, \
     ResourceRecord, ProcessDefinitionRecord
 from epu.processdispatcher.modes import RestartMode
-from epu.processdispatcher.util import get_process_state_message
+from epu.processdispatcher.util import get_process_state_message, \
+    get_set_difference_debug_message
 
 log = logging.getLogger(__name__)
 
@@ -1009,25 +1010,6 @@ class ProcessDispatcherCore(object):
             nodes[node_id] = dict(node)
 
         return state
-
-
-def get_set_difference_debug_message(set1, set2):
-    """Utility function for building log messages about set content changes
-    """
-    try:
-        difference1 = list(set1.difference(set2))
-        difference2 = list(set2.difference(set1))
-    except Exception, e:
-        return "can't calculate set difference. are these really sets?: %s" % str(e)
-
-    if difference1 and difference2:
-        return "removed=%s added=%s" % (difference1, difference2)
-    elif difference1:
-        return "removed=%s" % (difference1,)
-    elif difference2:
-        return "added=%s" % (difference2,)
-    else:
-        return "sets are equal"
 
 
 def _check_process_schedule_idempotency(process, parameters):

epu/processdispatcher/matchmaker.py
@@ -10,7 +10,8 @@ from epu.exceptions import WriteConflictError, NotFoundError
 from epu.states import ProcessState, ProcessDispatcherState, ExecutionResourceState
 from epu.processdispatcher.modes import QueueingMode
 from epu.processdispatcher.engines import domain_id_from_engine
-from epu.processdispatcher.util import get_process_state_message
+from epu.processdispatcher.util import get_process_state_message, \
+    get_set_difference
 
 log = logging.getLogger(__name__)
 
@@ -35,7 +36,8 @@ class PDMatchmaker(object):
 
     def __init__(self, core, store, resource_client, ee_registry, epum_client,
                  notifier, service_name, domain_definition_id,
-                 base_domain_config, run_type, restart_throttling_config):
+                 base_domain_config, run_type, restart_throttling_config,
+                 dispatch_retry_seconds=0):
         """
         @type core: ProcessDispatcherCore
         @type store: ProcessDispatcherStore
@@ -55,6 +57,9 @@ class PDMatchmaker(object):
         self.run_type = run_type
         self._cached_pd_state = None
         self.restart_throttling_config = restart_throttling_config
+
+        self.process_launcher = ProcessLauncher(store, resource_client, run_type,
+                                                retry_seconds=dispatch_retry_seconds)
 
         self.resources = None
         self.queued_processes = None
@@ -241,13 +246,21 @@ class PDMatchmaker(object):
         processes = self.store.get_queued_processes(
             watcher=self._notify_process_set_changed)
 
-        #TODO not really caring about priority or queue order
-        # at this point
-
         if processes != self.queued_processes:
+            added, removed = get_set_difference(set(self.queued_processes), set(processes))
+
+            if self.process_launcher.supports_retries and removed:
+                # find processes which were removed from queue and drop them from process launcher
+                for process_key in removed:
+                    if process_key in self.process_launcher:
+                        del self.process_launcher[process_key]
+
             log.debug("Queued process list has changed")
             self.queued_processes = processes
-            self.needs_matchmaking = True
+
+            if added:
+                # only need to matchmake when processes are added to queue
+                self.needs_matchmaking = True
 
     def _get_resource_set(self):
         self.resource_set_changed = False
@@ -284,7 +297,6 @@ class PDMatchmaker(object):
         for resource_id in changed:
             resource = self.store.get_resource(resource_id,
                                                watcher=self._notify_resource_changed)
-            #TODO fold in assignment vector in some fancy way?
             if resource:
                 self.resources[resource_id] = resource
 
@@ -325,10 +337,23 @@ class PDMatchmaker(object):
             if not self.needs_matchmaking and self.epum_client:
                 self.register_needs()
 
+            # retry any pending process launches
+            next_process_retry = None
+            if self.process_launcher.supports_retries:
+                next_process_retry = self.process_launcher.retry_process_dispatches()
+
             with self.condition:
                 if self.is_leader and not (self.resource_set_changed or
                         self.changed_resources or self.process_set_changed):
                     timeout = self._time_until_throttling_ends()
+
+                    # don't sleep as long if we anticipate retrying again soon
+                    if next_process_retry is not None:
+                        if timeout is None:
+                            timeout = next_process_retry
+                        else:
+                            timeout = min(timeout, next_process_retry)
+
                     if timeout > 0 or timeout is None:
                         self.condition.wait(timeout)
 
@@ -365,6 +390,12 @@ class PDMatchmaker(object):
         # don't rematch processes in ASSIGNED state -- we are awaiting
         # acknowledgment from an EE Agent
         if process.state == ProcessState.ASSIGNED:
+
+            # ensure process is under control of launcher retries --
+            # we could be recovering from a matchmaker failure
+            if self.process_launcher.supports_retries:
+                if process.key not in self.process_launcher:
+                    self.process_launcher.dispatch_process(process)
             return
 
         if self._throttle_end_time(process) > time.time():
@@ -423,11 +454,7 @@ class PDMatchmaker(object):
             process, matched_resource)
 
         if assigned:
-            try:
-                self._dispatch_process(process, matched_resource)
-            except Exception:
-                #TODO: this is not a good failure behavior
-                log.exception("Problem dispatching process from matchmaker")
+            self.process_launcher.dispatch_process(process)
 
         else:
             # backout resource record update if the process update failed due to
@@ -453,30 +480,6 @@ class PDMatchmaker(object):
                 break  # there can only be one match
 
         self._remove_queued_process(process.owner, process.upid, process.round)
-
-    def _dispatch_process(self, process, resource):
-        """Launch the process on a resource
-        """
-        definition = process.definition
-        executable = definition['executable']
-        # build up the spec form EE Agent expects
-        if self.run_type in ('pyon', 'pyon_single'):
-            parameters = dict(name=process.name,
-                module=executable['module'], cls=executable['class'],
-                module_uri=executable.get('url'))
-            config = _get_process_config(process)
-            if config is not None:
-                parameters['config'] = config
-        elif self.run_type == 'supd':
-            parameters = executable
-        else:
-            msg = "Don't know how to format parameters for '%s' run type" % self.run_type
-            log.warning(msg)
-            parameters = {}
-
-        self.resource_client.launch_process(
-            resource.resource_id, process.upid, process.round,
-            self.run_type, parameters)
 
     def _maybe_update_assigned_process(self, process, resource):
         updated = False
@@ -860,6 +863,102 @@ def _get_process_config(process):
         return config
 
     return None
+
+
+class ProcessLauncher(object):
+    def __init__(self, store, resource_client, run_type, retry_seconds=0):
+        self.store = store
+        self.resource_client = resource_client
+        self.run_type = run_type
+        if retry_seconds and retry_seconds > 0:
+            self.retry_seconds = float(retry_seconds)
+            self.supports_retries = True
+        else:
+            self.supports_retries = False
+            self.retry_seconds = 0
+
+        # map of pending process owner/ID pairs to last dispatch times
+        self.pending_process_dispatches = {}
+
+    def __contains__(self, item):
+        return item in self.pending_process_dispatches
+
+    def __delitem__(self, item):
+        del self.pending_process_dispatches[item]
+
+    def dispatch_process(self, process):
+        """Launches a process onto a resource.
+
+        If retries are enabled, the process will be queued for retry
+        until it is acknowledged or canceled.
+        """
+        if self.retry_seconds:
+            self.pending_process_dispatches[process.key] = time.time()
+
+        self._do_dispatch(process)
+
+    def _do_dispatch(self, process):
+        assert process.state == ProcessState.ASSIGNED
+        resource_id = process.assigned
+        parameters = self._get_process_parameters(process)
+        try:
+            log.info("Dispatching process %s (round %s) to EEAgent resource %s",
+                     process.upid, process.round, resource_id)
+            self.resource_client.launch_process(
+                resource_id, process.upid, process.round,
+                self.run_type, parameters)
+        except Exception:
+            log.warn("Problem dispatching process %s to EEAgent %s. Will retry.",
+                process.upid, resource_id, exc_info=True)
+
+    def _get_process_parameters(self, process):
+        definition = process.definition
+        executable = definition['executable']
+        if self.run_type in ('pyon', 'pyon_single'):
+            parameters = dict(name=process.name,
+                module=executable['module'], cls=executable['class'],
+                module_uri=executable.get('url'))
+            config = _get_process_config(process)
+            if config is not None:
+                parameters['config'] = config
+        elif self.run_type == 'supd':
+            parameters = executable
+        else:
+            msg = "Don't know how to format parameters for '%s' run type" % self.run_type
+            log.warning(msg)
+            parameters = {}
+        return parameters
+
+    def retry_process_dispatches(self):
+        """Retries unacknowledged dispatch requests older than retry_seconds
+
+        Returns seconds until next retry
+        """
+        global_next_retry = None
+        now = time.time()
+        for process_key, last_time in self.pending_process_dispatches.items():
+            next_retry = last_time + self.retry_seconds
+            if next_retry <= now:
+                owner, upid, round = process_key
+                process = self.store.get_process(owner, upid)
+                if not (process and process.round == round
+                        and process.state == ProcessState.ASSIGNED):
+                    # if the process has moved on to another round or state,
+                    # drop it from our set and move on
+                    del self.pending_process_dispatches[process_key]
+                    continue
+
+                # otherwise, re-attempt the request
+                self._do_dispatch(process)
+                self.pending_process_dispatches[process_key] = now + self.retry_seconds
+            else:
+                if global_next_retry is None or next_retry < global_next_retry:
+                    global_next_retry = next_retry
+
+        if global_next_retry is None:
+            return None
+
+        return max(global_next_retry - time.time(), 0)
 
 
 class NodeContainer(object):

epu/processdispatcher/test/test_matchmaker.py
@@ -66,6 +66,7 @@ class PDMatchmakerTests(unittest.TestCase, StoreTestMixin):
         self.base_domain_config = get_domain_config()
         self.run_type = "fake_run_type"
         self.restart_throttling_config = {}
+        self.dispatch_retry_seconds = 30
 
         self.core = ProcessDispatcherCore(self.store, self.registry,
             self.resource_client, self.notifier)
@@ -74,7 +75,7 @@ class PDMatchmakerTests(unittest.TestCase, StoreTestMixin):
         self.mm = PDMatchmaker(self.core, self.store, self.resource_client,
             self.registry, self.epum_client, self.notifier, self.service_name,
             self.definition_id, self.base_domain_config, self.run_type,
-            self.restart_throttling_config)
+            self.restart_throttling_config, self.dispatch_retry_seconds)
 
         self.mmthread = None
 
@@ -589,11 +590,41 @@ class PDMatchmakerTests(unittest.TestCase, StoreTestMixin):
         self.mm._get_queued_processes()
         self.mm._get_resource_set()
 
+        self.mm.process_launcher.pending_process_dispatches[p1.key] = time.time()
+
         # kinda dirty: patch out an internal MM function and ensure it isn't called
         with patch.object(self.mm, '_handle_matched_process') as m:
             self.mm.matchmake()
 
             assert not m.called, "mm has calls: %s" % (m.call_args,)
+
+        self.assertEqual(len(self.mm.process_launcher.pending_process_dispatches), 1)
+
+    def test_assigned_process_not_rematched_but_retried(self):
+        # Processes are moved to the ASSIGNED state when assigned by the matchmaker.
+        # Though they remain in the queue, they should not be rematched
+        p1 = ProcessRecord.new(None, "p1", get_process_definition(),
+            ProcessState.ASSIGNED, assigned="r1")
+        self.store.add_process(p1)
+        self.store.enqueue_process(*p1.key)
+
+        props = {"engine": "engine1"}
+        r1 = ResourceRecord.new("r1", "n1", 1, properties=props)
+        r1.assigned.append(p1.key)
+        self.store.add_resource(r1)
+
+        self.mm.initialize()
+        # sneak into MM and force it to update this info from the store
+        self.mm._get_queued_processes()
+        self.mm._get_resource_set()
+
+        # kinda dirty: patch out an internal MM function and ensure it isn't called
+        with patch.object(self.mm, '_handle_matched_process') as m:
+            self.mm.matchmake()
+
+            assert not m.called, "mm has calls: %s" % (m.call_args,)
+
+        self.assertIn(p1.key, self.mm.process_launcher.pending_process_dispatches)
 
     def test_wait_resource(self):
         props = {"engine": "engine1"}

epu/processdispatcher/test/test_processdispatcher_service.py
@@ -7,7 +7,7 @@ import uuid
 import threading
 from socket import timeout
 
-
+from mock import patch
 from dashi import bootstrap, DashiConnection
 
 import epu.tevent as tevent
@@ -1652,6 +1652,36 @@ class ProcessDispatcherServiceTests(unittest.TestCase):
         self.assertEqual(terminated_history['u_pid'], 'p1')
         self.assertEqual(terminated_history['state'], ProcessState.TERMINATED)
         self.assertEqual(terminated_history['round'], 0)
+
+    def test_matchmaker_msg_retry(self):
+        node = "node1"
+        domain_id = domain_id_from_engine('engine1')
+        self.client.node_state(node, domain_id, InstanceState.RUNNING)
+        self._spawn_eeagent(node, 1)
+
+        # sneak in and shorten retry time
+        self.pd.matchmaker.process_launcher.retry_seconds = 0.5
+
+        proc1 = "proc1"
+        proc2 = "proc2"
+
+        with patch.object(self.pd.matchmaker.process_launcher, "resource_client") as mock_resource_client:
+            # first process request goes to mock, not real client but no error
+            self.client.schedule_process(proc1, self.process_definition_id)
+
+            # second process hits an error but that should not cause problems
+            mock_resource_client.launch_process.side_effect = Exception("boom!")
+            self.client.schedule_process(proc2, self.process_definition_id)
+
+            self._wait_assert_pd_dump(self._assert_process_states,
+                                      ProcessState.ASSIGNED, [proc1, proc2])
+            self.assertEqual(mock_resource_client.launch_process.call_count, 2)
+
+        # now resource client works again. those messages should be retried
+        self.notifier.wait_for_state(proc1, ProcessState.RUNNING)
+        self.notifier.wait_for_state(proc2, ProcessState.RUNNING)
+        self._wait_assert_pd_dump(self._assert_process_states,
+                                  ProcessState.RUNNING, [proc1, proc2])
 
 
 class ProcessDispatcherServiceZooKeeperTests(ProcessDispatcherServiceTests, ZooKeeperTestMixin):

commit b6a1886a19ba903ddda7f7c4af166e12b7c274c0

Fix PD matchmaker bug introduced in 8a87641870639

epu/processdispatcher/util.py
@@ -25,8 +25,8 @@ def get_process_state_message(process):
 def get_set_difference(set1, set2):
     """Returns a tuple of lists (added, removed)
     """
-    added = list(set1.difference(set2))
-    removed = list(set2.difference(set1))
+    added = list(set2.difference(set1))
+    removed = list(set1.difference(set2))
     return added, removed
 
 