5194c97722bdbbbb6c16450fae56ef82dbb16aa8
Patrick Armstrong <patricka@uvic.ca>
2013-07-15 10:22:51 -0700

    Tidy setup.py

--- setup.py
+++ setup.py
@@ -15,16 +15,16 @@ if sys.platform == 'darwin':
 version = '1.2.2-dev'
 
 setupdict = {
-    'name' : 'epu',
-    'version' : version,
-    'description' : 'OOICI CEI Elastic Processing Unit Services and Agents',
+    'name': 'epu',
+    'version': version,
+    'description': 'OOICI CEI Elastic Processing Unit Services and Agents',
     'url': 'https://confluence.oceanobservatories.org/display/CIDev/Common+Execution+Infrastructure+Development',
-    'download_url' : 'http://sddevrepo.oceanobservatories.org/releases',
-    'license' : 'Apache 2.0',
-    'author' : 'CEI',
-    'author_email' : 'nimbus@mcs.anl.gov',
-    'keywords': ['ooici','cei','epu'],
-    'classifiers' : [
+    'download_url': 'http://sddevrepo.oceanobservatories.org/releases',
+    'license': 'Apache 2.0',
+    'author': 'CEI',
+    'author_email': 'nimbus@mcs.anl.gov',
+    'keywords': ['ooici', 'cei', 'epu'],
+    'classifiers': [
     'Development Status :: 3 - Alpha',
     'Environment :: Console',
     'Intended Audience :: Developers',
@@ -52,22 +52,22 @@ setupdict['install_requires'] = ['httplib2>=0.7.1',
                                  ]
 setupdict['tests_require'] = ['epuharness', 'nose', 'mock']
 setupdict['extras_require'] = {
-        'test': setupdict['tests_require'],
-        'exceptional': ['exceptional-python'],
-        'statsd': ['statsd'],
-        }
+    'test': setupdict['tests_require'],
+    'exceptional': ['exceptional-python'],
+    'statsd': ['statsd'],
+}
 setupdict['test_suite'] = 'nose.collector'
 
 setupdict['entry_points'] = {
-        'console_scripts': [
-            'epu-management-service=epu.dashiproc.epumanagement:main',
-            'epu-provisioner-service=epu.dashiproc.provisioner:main',
-            'epu-processdispatcher-service=epu.dashiproc.processdispatcher:main',
-            'epu-zktool=epu.zkcli:main',
-            'epu-high-availability-service=epu.dashiproc.highavailability:main',
-            'epu-dtrs=epu.dashiproc.dtrs:main',
-            ]
-        }
+    'console_scripts': [
+        'epu-management-service=epu.dashiproc.epumanagement:main',
+        'epu-provisioner-service=epu.dashiproc.provisioner:main',
+        'epu-processdispatcher-service=epu.dashiproc.processdispatcher:main',
+        'epu-zktool=epu.zkcli:main',
+        'epu-high-availability-service=epu.dashiproc.highavailability:main',
+        'epu-dtrs=epu.dashiproc.dtrs:main',
+    ]
+}
 setupdict['scripts'] = ["scripts/epu-process"]
 
 setupdict['package_data'] = {'epu': ['config/*.yml']}
