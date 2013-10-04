19a3b85b723f82ea1d9055534eb1818f3e11c7b4
Patrick Armstrong <patricka@uvic.ca>
2013-10-04 14:51:25 -0400

    Update index in opentsdb query to support phantom_unique

--- epu/sensors/opentsdb.py
+++ epu/sensors/opentsdb.py
@@ -69,6 +69,8 @@ class OpenTSDB(ISensorAggregator):
         # TODO: this could be process etc in future
         if 'domain' in dimensions:
             index = 'domain'
+        if 'phantom_unique' in dimensions:
+            index = 'phantom_unique'
         else:
             index = 'host'
 
