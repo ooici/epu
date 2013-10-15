199f79e293a8bb2fd73706e303aa076654c34214
Patrick Armstrong <patricka@uvic.ca>
2013-10-15 15:51:32 -0400

    Tidy setup.py and setup.cfg

--- setup.py
+++ setup.py
@@ -1,64 +1,33 @@
-#!/usr/bin/env python
-
-"""
-@file setup.py
-@see http://peak.telecommunity.com/DevCenter/setuptools
-"""
-
-import sys
-import os
-
-# Add /usr/local/include to the path for macs, fixes easy_install for several packages (like gevent and pyyaml)
-if sys.platform == 'darwin':
-    os.environ['C_INCLUDE_PATH'] = '/usr/local/include'
+#!/usr/bin/env python -w
+import os, sys
+from setuptools import setup, find_packages
+homebrew_include_path = '/usr/local/include'
+if sys.platform == 'darwin' and os.path.isdir(homebrew_include_path):
+    os.environ['C_INCLUDE_PATH'] = homebrew_include_path
 
 version = '2.1.1'
-
-setupdict = {
-    'name': 'epu',
-    'version': version,
-    'description': 'OOICI CEI Elastic Processing Unit Services and Agents',
-    'url': 'https://confluence.oceanobservatories.org/display/CIDev/Common+Execution+Infrastructure+Development',
-    'download_url': 'http://sddevrepo.oceanobservatories.org/releases',
-    'license': 'Apache 2.0',
-    'author': 'CEI',
-    'author_email': 'nimbus@mcs.anl.gov',
-    'keywords': ['ooici', 'cei', 'epu'],
-    'classifiers': [
-    'Development Status :: 3 - Alpha',
-    'Environment :: Console',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: Apache Software License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Topic :: Scientific/Engineering'],
-}
-
-from setuptools import setup, find_packages
-setupdict['packages'] = find_packages()
-
-setupdict['dependency_links'] = ['http://sddevrepo.oceanobservatories.org/releases']
-setupdict['test_suite'] = 'epu'
-
-setupdict['install_requires'] = ['httplib2>=0.7.1',
-                                  'boto >= 2.6',
-                                  'apache-libcloud==0.11.1',
-                                  'kazoo==1.2.1',
-                                  'dashi>=0.2.1',
-                                  'gevent>=0.13.7',
-                                  'simplejson',
-                                  'pychef',
-                                  'mock'
-                                 ]
-setupdict['tests_require'] = ['epuharness', 'nose', 'mock']
-setupdict['extras_require'] = {
-    'test': setupdict['tests_require'],
+requires = [
+    'httplib2>=0.7.1',
+    'boto >= 2.6',
+    'apache-libcloud==0.11.1',
+    'kazoo==1.2.1',
+    'dashi>=0.2.1',
+    'gevent>=0.13.7',
+    'simplejson',
+    'pychef',
+    'mock'
+]
+tests_require = [
+    'epuharness',
+    'nose',
+    'mock'
+]
+extras_require = {
+    'test': tests_require,
     'exceptional': ['exceptional-python'],
     'statsd': ['statsd'],
 }
-setupdict['test_suite'] = 'nose.collector'
-
-setupdict['entry_points'] = {
+entry_points = {
     'console_scripts': [
         'epu-management-service=epu.dashiproc.epumanagement:main',
         'epu-provisioner-service=epu.dashiproc.provisioner:main',
@@ -68,8 +37,41 @@ setupdict['entry_points'] = {
         'epu-dtrs=epu.dashiproc.dtrs:main',
     ]
 }
-setupdict['scripts'] = ["scripts/epu-process"]
+scripts = [
+    "scripts/epu-process"
+]
+package_data = {
+    'epu': [
+        'config/*.yml'
+    ]
+}
 
-setupdict['package_data'] = {'epu': ['config/*.yml']}
-
-setup(**setupdict)
+setup(
+    name='epu',
+    version=version,
+    description='OOICI CEI Elastic Processing Unit Services and Agents',
+    url='https://confluence.oceanobservatories.org/display/CIDev/Common+Execution+Infrastructure+Development',
+    download_url='http://sddevrepo.oceanobservatories.org/releases',
+    license='Apache 2.0',
+    author='CEI',
+    author_email='nimbus@mcs.anl.gov',
+    keywords=['ooici', 'cei', 'epu'],
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering'
+    ),
+    packages=find_packages(),
+    dependency_links=['http://sddevrepo.oceanobservatories.org/releases'],
+    install_requires=requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
+    test_suite='nose.collector',
+    entry_points=entry_points,
+    scripts=scripts,
+    package_data=package_data,
+)
